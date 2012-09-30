e84143e2ca19fc664674a3e108b7385d6942a236
Matt Venn <matt@mattvenn.net>
2012-09-30 12:24:02 +0100

    not much

--- portforward/pf.py
+++ portforward/pf.py
@@ -1,16 +1,17 @@
 from twisted.internet import reactor
 from twisted.protocols import portforward
 
-"""
 class LoggingProxyServer(portforward.ProxyServer):
     def dataReceived(self, data):
         portforward.ProxyServer.dataReceived(self, data)
+        print data
 
 class LoggingProxyFactory(portforward.ProxyFactory):
     protocol = LoggingProxyServer
-"""
+
 if __name__ == '__main__':
-    #fwd = portforward.ProxyFactory('192.168.2.150', 502)
-    fwd = portforward.ProxyFactory('192.168.2.150', 80)
+    #fwd = portforward.LoggingProxyFactory('192.168.2.150', 502)
+    fwd = portforward.ProxyFactory('192.168.2.150', 502)
+    #fwd = portforward.ProxyFactory('192.168.2.150', 80)
     reactor.listenTCP(8080, fwd)
     reactor.run()
