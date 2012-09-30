ac1c1cb04a864539904e840a4e7f0d25fd6c0329
Matt Venn <matt@mattvenn.net>
2012-09-30 20:11:20 +0100

    removed old bits

--- portforward/pf.py
+++ portforward/pf.py
@@ -1,18 +1,7 @@
 from twisted.internet import reactor
 from twisted.protocols import portforward
 
-class LoggingProxyServer(portforward.ProxyServer):
-    def dataReceived(self, data):
-	print data
-        portforward.ProxyServer.dataReceived(self, data)
-
-class LoggingProxyFactory(portforward.ProxyFactory):
-    protocol = LoggingProxyServer
-
 if __name__ == '__main__':
-   #fwd = LoggingProxyFactory('192.168.2.150', 502)
-   #fwd = LoggingProxyFactory('192.168.2.150', 80)
    fwd = portforward.ProxyFactory('192.168.2.150', 502)
-   #fwd = portforward.ProxyFactory('192.168.2.150', 80)
    reactor.listenTCP(8080, fwd)
    reactor.run()
